6ebf764b5e26b7bd348dc4a4166a22da071afe07
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-05-02 10:40:47 -0600

    Creates express file and get endpoints

---
+++ server.js
@@ -0,0 +1,68 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+const express = require('express');
+const app = express();
+
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Erupt';
+
+app.get('/api/v1/volcanoes', (request, response) => {
+  database('volcano').select()
+    .then((volcano) => {
+      response.status(200).json(volcano);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/volcanoes/:id', (req, res) => {
+  database('volcano').where('id', req.params.id).select()
+    .then(volcano => {
+      res.status(200).json(volcano)
+      if (volcano.length) {
+        res.status(200).json(volcano);
+      } else {
+        res.status(404).json({
+          error: `Could not find specified volcano.`
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/eruptions', (request, response) => {
+  database('eruption').select()
+    .then((eruption) => {
+      response.status(200).json(eruption);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/eruptions/:id', (req, res) => {
+  database('eruption').where('id', req.params.id).select()
+    .then(eruption => {
+      res.status(200).json(eruption)
+      if (eruption.length) {
+        res.status(200).json(eruption);
+      } else {
+        res.status(404).json({
+          error: `Could not find specified eruption.`
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error });
+    });
+});
+
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
