403172d3e8bff2487c5b0667a89c4eeecee55f07
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-05-03 09:38:09 -0600

    Configure

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
+const environment = process.env.NODE_ENV || 'development';
 const database = require('knex')(configuration);
 const express = require('express');
 const app = express();
