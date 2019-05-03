e286e81b48a7c454b86d98de7afe95ce76beb46e
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-05-03 09:45:32 -0600

    Port2

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || 'development';
+
 const configuration = require('./knexfile')[environment];
 const environment = process.env.NODE_ENV || 'development';
 const database = require('knex')(configuration);
